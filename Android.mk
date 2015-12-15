6adaf9165837f461c6c4f28451e7e9e85f24ae11
vm03 <vm03@yandex.ru>
2015-12-15 12:25:34 +0300

    jag3gds: typo

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 #
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter w7 jag3gd jagnm g2m w5 w3ds,$(TARGET_DEVICE)),)
+ifneq ($(filter w7 jag3gds jagnm g2m w5 w3ds,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
