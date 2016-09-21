e978cd3e0a7a37fbd4265b1d37017b3a76ed92c6
Nikita <nikich340@gmail.com>
2016-09-21 21:20:01 +1000

    Sort camera parameters alphabetically
    Change-Id: Ie417f8b003fa602937e19ed280be42c8cac9b61d

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -17,10 +17,12 @@
 #define CAMERA_PARAMETERS_EXTRA_C \
 const char CameraParameters::AUDIO_ZOOM_OFF[] = "audio-zoom"; \
 const char CameraParameters::AUDIO_ZOOM_ON[] = "audio-zoom"; \
+const char CameraParameters::AUTO_HDR_SUPPORTED[] = "auto-hdr-supported"; \
 const char CameraParameters::BEAUTY_SHOT_OFF[] = "beauty-shot"; \
 const char CameraParameters::BEAUTY_SHOT_ON[] = "beauty-shot"; \
 const char CameraParameters::BURST_SHOT_OFF[] = "burst-shot"; \
 const char CameraParameters::BURST_SHOT_ON[] = "burst-shot"; \
+const char CameraParameters::DENOISE_ON_OFF_MODES_MAP[] = "denoise-values"; \
 const char CameraParameters::KEY_AUDIO_ZOOM[] = "audio-zoom"; \
 const char CameraParameters::KEY_AUDIO_ZOOM_SUPPORTED[] = "audio-zoom-supported"; \
 const char CameraParameters::KEY_BEAUTY_SHOT[] = "beauty-shot"; \
@@ -29,27 +31,27 @@ const char CameraParameters::KEY_BURST_SHOT[] = "burst-shot"; \
 const char CameraParameters::KEY_BURST_SHOT_SUPPORTED[] = "burst-shot-supported"; \
 const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING[] = "object-tracking"; \
 const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[] = "object-tracking-supported"; \
-const char CameraParameters::KEY_VIDEO_WDR[] = "video-wdr"; \
-const char CameraParameters::KEY_VIDEO_WDR_SUPPORTED[] = "video-wdr-supported"; \
-const char CameraParameters::VIDEO_WDR_OFF[] = "video-wdr"; \
-const char CameraParameters::VIDEO_WDR_ON[] = "video-wdr"; \
-const char CameraParameters::OBJECT_TRACKING_ON[] = "object-tracking"; \
-const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking"; \
+const char CameraParameters::KEY_HDR_MODE[] = "hdr-mode"; \
 const char CameraParameters::KEY_LGE_CAMERA[] = "lge-camera"; \
 const char CameraParameters::KEY_QC_DENOISE[] = "denoise"; \
-const char CameraParameters::DENOISE_ON_OFF_MODES_MAP[] = "denoise-values"; \
-const char CameraParameters::KEY_HDR_MODE[] = "hdr-mode"; \
-const char CameraParameters::AUTO_HDR_SUPPORTED[] = "auto-hdr-supported"; \
 const char CameraParameters::KEY_QC_SUPPORTED_AE_BRACKETING_MODES[] = "ae-bracket-hdr-values"; \
-const char CameraParameters::KEY_QC_ZSL[] = "zsl";
+const char CameraParameters::KEY_QC_ZSL[] = "zsl"; \
+const char CameraParameters::KEY_VIDEO_WDR[] = "video-wdr"; \
+const char CameraParameters::KEY_VIDEO_WDR_SUPPORTED[] = "video-wdr-supported"; \
+const char CameraParameters::OBJECT_TRACKING_ON[] = "object-tracking"; \
+const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking"; \
+const char CameraParameters::VIDEO_WDR_OFF[] = "video-wdr"; \
+const char CameraParameters::VIDEO_WDR_ON[] = "video-wdr";
 
 #define CAMERA_PARAMETERS_EXTRA_H \
     static const char AUDIO_ZOOM_OFF[]; \
     static const char AUDIO_ZOOM_ON[]; \
+    static const char AUTO_HDR_SUPPORTED[]; \
     static const char BEAUTY_SHOT_OFF[]; \
     static const char BEAUTY_SHOT_ON[]; \
     static const char BURST_SHOT_OFF[]; \
     static const char BURST_SHOT_ON[]; \
+    static const char DENOISE_ON_OFF_MODES_MAP[]; \
     static const char KEY_AUDIO_ZOOM[]; \
     static const char KEY_AUDIO_ZOOM_SUPPORTED[]; \
     static const char KEY_BEAUTY_SHOT[]; \
@@ -58,17 +60,15 @@ const char CameraParameters::KEY_QC_ZSL[] = "zsl";
     static const char KEY_BURST_SHOT_SUPPORTED[]; \
     static const char KEY_FOCUS_MODE_OBJECT_TRACKING[]; \
     static const char KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[]; \
-    static const char KEY_VIDEO_WDR[]; \
-    static const char KEY_VIDEO_WDR_SUPPORTED[]; \
-    static const char VIDEO_WDR_OFF[]; \
-    static const char VIDEO_WDR_ON[]; \
-    static const char OBJECT_TRACKING_ON[]; \
-    static const char OBJECT_TRACKING_OFF[]; \
+    static const char KEY_HDR_MODE[]; \
     static const char KEY_LGE_CAMERA[]; \
     static const char KEY_QC_DENOISE[]; \
-    static const char DENOISE_ON_OFF_MODES_MAP[]; \
-    static const char KEY_HDR_MODE[]; \
-    static const char AUTO_HDR_SUPPORTED[]; \
     static const char KEY_QC_SUPPORTED_AE_BRACKETING_MODES[]; \
-    static const char KEY_QC_ZSL[];
+    static const char KEY_QC_ZSL[]; \
+    static const char KEY_VIDEO_WDR[]; \
+    static const char KEY_VIDEO_WDR_SUPPORTED[]; \
+    static const char OBJECT_TRACKING_ON[]; \
+    static const char OBJECT_TRACKING_OFF[]; \
+    static const char VIDEO_WDR_OFF[]; \
+    static const char VIDEO_WDR_ON[];
 
